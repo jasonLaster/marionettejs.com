e8c5123facf62d522270e1cace9f4d1fa1191868
Sam Saccone <samccone@gmail.com>
2014-10-06 23:42:14 -0400

    deploy

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    // Cycle 2
+    $('.video_slideshow').cycle({
+        pager: '.slider2',
+        pagerTemplate: '<span></span>',
+        paused: true
+    });
+
+    // equalHeights
+    if($(window).width() > 705) {
+        $('.code_examples > div').equalHeights();
+    }
+    $(window).resize(function() {
+        if($(window).width() > 705) {
+            $('.code_examples > div').css('height', 'auto');
+            $('.code_examples > div').equalHeights();
+        } else {
+            $('.code_examples > div').css('height', 'auto');
+        }
+    });
+
+    // Google Prettify syntax highlighting
+    prettyPrint();
+
+    // Toggle company logos
+    $('.show-more').on('click', function(e) {
+      e.preventDefault();
+      $(this).parent().toggleClass('active');
+      $('.slider').toggleClass('closed');
+    });
+});
